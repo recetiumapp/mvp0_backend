0962b078bcb5398bc31a67b6354f3725bbc9c1f7
Rob Data <roberthernandezmarketing@gmail.com>
2025-10-01 08:39:49 -0400

    render deployment

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export PYTHONPATH=/opt/render/project/src/src:$PYTHONPATH
+export PYTHONPATH=./src:$PYTHONPATH
 gunicorn main:app --chdir src --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
